981392e3cd9c7cd32f81e71ff6d6f9a38537b67b
Antony rossi de jesus <antonyrossidejesus@gmail.com>
2024-06-17 22:58:16 -0300

    atualizando json

--- next.config.mjs
+++ next.config.mjs
@@ -2,3 +2,10 @@
 const nextConfig = {};
 
 export default nextConfig;
+
+const isProd = process.env.NODE_ENV === "production";
+
+module.exports = {
+  assetPrefix: isProd ? "/instasany/" : "",
+  basePath: isProd ? "/instasany" : "",
+};
